30b01314611123b5f535dbb16123d4772304957c
devashish mishra <mishradevashish931@gmail.com>
2026-02-12 23:48:46 +0530

    updated

---
+++ calculate1.js/5sync.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+var a=18744;
+var b=33432;
+
+function multiply(x,y){
+    const result=a*b;
+    return result;
+}
+var c=multiply(a,b);
+
+console.log("Multiplication result is: ", c);
+
---
+++ calculate1.js/6Async.js
@@ -0,0 +1,21 @@
+const fs=require("fs");
+
+const https=require("https");
+
+console.log("hello world");
+
+var a=1078698;
+var b=20986;
+
+https.get("https://dummyjson.com/products/1",(res)=>{
+    console.log("Fetched data Successfully");
+
+});
+
+setTimeout(()=>{
+    console.log("setTimeout called after 5 second");
+},5000);
+
+fs.readFile("./7file.txt","utf8", (err,data)=>{
+    console.log("file Data :", data);
+    });
