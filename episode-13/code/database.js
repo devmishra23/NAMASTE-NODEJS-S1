997491f9b8458852f92927e6f88117cba0071330
devashish mishra <mishradevashish931@gmail.com>
2026-03-22 15:11:14 +0530

    Restructured project and added gitignore

---
+++ episode-13/code/database.js
@@ -0,0 +1,16 @@
+const URI="mongodb+srv://NamasteDev:dev12345@namastenode.zxagkvj.mongodb.net/users"
+
+
+//GO TO mongoDB website
+//Create a free M0 cluster
+//Create a user
+//Get the connection string
+//Install MongoDB compass
+
+
+//TO CONNECT MONGODB TO VS CODE 
+/*
+ Step 1-> Need an NPM Package
+Step 2-->  
+
+*/
--- episode-13/database.js
+++
@@ -1,4 +0,0 @@
-const URI="mongodb+srv://NamasteDev:dev12345@namastenode.zxagkvj.mongodb.net/users"
-
-//TO CONNECT MONGODB TO VS CODE 
-
